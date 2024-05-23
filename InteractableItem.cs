306d66f4d3c25e1ba0d7df294b50646712cfb3c3
MrTibor <147601629+MrTibor@users.noreply.github.com>
2024-05-23 14:33:41 +0200

    Add files via upload

---
+++ InteractableItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// Base class
+public class InteractableItem : MonoBehaviour
+
+{
+
+
+    public AddPoints addPointsScript; //help with chatgpt
+
+    protected virtual void Start()
+    
+    {
+        // Find the AddPoints script in the scene
+        addPointsScript = GameObject.FindObjectOfType<AddPoints>();
+    
+    
+    }
+
+    protected virtual void OnMouseDown()
+    {
+        // Base OnMouseDown behavior
+    
+    
+    
+    
+    }
+}
